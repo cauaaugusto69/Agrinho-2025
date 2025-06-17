33f3c81f5067cd7daec7ea75deaadc9724f7fa47
cauaaugusto69 <caua.vilhalba@escola.pr.gov.br>
2025-06-17 08:24:22 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var img01;
+
+var xJogador1=800;
+var xJogador2=700;
+var xJogador3=0;
+var xJogador4=0;
+
+function setup() {
+  createCanvas(800, 600);
+img01= loadImage('cidade-campo-_PRINCIPAL-768x569.jpg');
+
+}
+  
+  function draw() {
+  background(img01);
+  
+ textSize(60);
+ text("👨‍🦽", xJogador1,270);  
+ xJogador1  -= random(1);
+  
+
+   text("🦅", xJogador2,380);  
+   xJogador2  -= random(1);
+   
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
